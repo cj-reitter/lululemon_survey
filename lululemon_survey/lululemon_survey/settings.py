736480a09f8591a22e8142a07f6ebef82e2a4ec6
cj-reitter <carreitter@gmail.com>
2026-05-02 13:27:14 +0000

    Fixed bug in url name in settings.py

--- lululemon_survey/lululemon_survey/settings.py
+++ lululemon_survey/lululemon_survey/settings.py
@@ -19,9 +19,12 @@ SECURE_HSTS_SECONDS = os.getenv('SECURE_HSTS_SECONDS')
 SECURE_HSTS_INCLUDE_SUBDOMAINS = os.getenv('SECURE_HSTS_INCLUDE_SUBDOMAINS') != 'False'
 SECURE_HSTS_PRELOAD = os.getenv('SECURE_HSTS_PRELOAD') != 'False'
 
-ALLOWED_HOSTS = ['.lu-lululemon-survey', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    '.lu-lululemon-survey.com',
+    '127.0.0.1',
+    'localhost']
 CSRF_TRUSTED_ORIGINS = [
-    'https://lu-lululemon-survey.com',
+    'https://*.lu-lululemon-survey.com',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
 ]
